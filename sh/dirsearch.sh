48cba768e0ce15acdffb7c9ce6a1ce5fa61c861f
kitty5kull <90086425+kitty5kull@users.noreply.github.com>
2022-11-03 10:22:44 +0100

    Dirsearch and webscan updates

--- sh/dirsearch.sh
+++ sh/dirsearch.sh
@@ -8,4 +8,5 @@ then
 	exit
 fi
 
-dirsearch -u "$1" -o "$2" --format=simple --wordlist=/home/kitty/wordlists/dirb/common.txt -e html,htm,php,asp,aspx,js,txt,old,bak,cfg,conf,jsp,do,action,zip,tar,gz -f -r -R 3 "{$*:3}"
+#dirsearch -u "$1" -o "$2" --format=simple --wordlist=/home/kitty/wordlists/dirb/common.txt -e html,htm,php,asp,aspx,js,txt,old,bak,cfg,conf,jsp,do,action,zip,tar,gz -f -r -R 3 "{$*:3}"
+dirsearch -u "$1" -o "$2" --format=plain -e html,htm,php,asp,aspx,js,txt,old,bak,cfg,conf,jsp,do,action,zip,tar,gz -f -r -R 3 -x 400 "{$*:3}"
--- sh/webscan.sh
+++ sh/webscan.sh
@@ -8,16 +8,26 @@ fi
 mkdir webscan
 cd webscan
 
+wget $1/robots.txt
+wget $1/security.txt
+wget $1/sitemap.xml
+
+mousepad robots.txt security.txt sitemap.xml &
+
 /opt/gobuster/gobuster dir -u $1 -k -w ~/wordlists/dirb/big.txt | tee gobuster_dir_big.txt
 /opt/gobuster/gobuster vhost -u $1 -k -w ~/wordlists/Discovery/DNS/subdomains-top1million-20000.txt | tee gobuster_vhost_20k.txt
-/opt/nikto/program/nikto.pl --url=$1 | tee nikto.txt
 dirsearch.sh $1 dirsearch.txt
 
 cewl --with-numbers -w cewl.txt $1
 grep -v "\/$" dirsearch.txt | downall.sh
 grep -Eroh "[0-9a-zA-Z]*" cewl.txt dl/*.txt | sort | uniq > ../wordlist.txt
-dirsearch -u "$1" -o dirsearch_cewl.txt --format=simple --wordlist=../wordlist.txt -e html,htm,php,asp,aspx,js,txt,old,bak,cfg,conf,jsp,do,action,zip,tar,gz -f -r -R 3
+dirsearch -u "$1" -o dirsearch_cewl.txt --format=plain --wordlist=../wordlist.txt -e html,htm,php,asp,aspx,js,txt,old,bak,cfg,conf,jsp,do,action,zip,tar,gz -f -r -R 3
 
-cat gobuster_dir_big.txt nikto.txt dirsearch.txt dirsearch_cewl.txt webscan.txt
+cat dir*.txt | grep "^200" | cut -b 14- > direarch_clean.txt
+
+/opt/nikto/program/nikto.pl --url=$1 | tee nikto.txt
+
+cat gobuster_dir_big.txt nikto.txt dirsearch.txt dirsearch_cewl.txt > webscan.txt
 mousepad webscan.txt &
+
 cd ..
